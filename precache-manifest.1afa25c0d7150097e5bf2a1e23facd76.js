a66715b884c2935ece5711b6ef862fe3c05374d9
Ionut Alixandroae <ialixandroae@yahoo.com>
2022-02-06 11:42:20 +0100

    Updates

---
+++ precache-manifest.1afa25c0d7150097e5bf2a1e23facd76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0132dd43f645c93922fc96de246215",
+    "url": "/pic-maps/index.html"
+  },
+  {
+    "revision": "471642a96d64cb4f5d4d",
+    "url": "/pic-maps/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "c3245caf81985793e3f9",
+    "url": "/pic-maps/static/css/main.84fc38d3.chunk.css"
+  },
+  {
+    "revision": "471642a96d64cb4f5d4d",
+    "url": "/pic-maps/static/js/2.6df7d8fd.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/pic-maps/static/js/2.6df7d8fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3245caf81985793e3f9",
+    "url": "/pic-maps/static/js/main.55200a52.chunk.js"
+  },
+  {
+    "revision": "81314ec9322d9a75ded5",
+    "url": "/pic-maps/static/js/runtime-main.6f1d73bf.js"
+  }
+]);
